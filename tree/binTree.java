e5792c670d8806ea7169db5bb47948dcc16b50b4
Miaoshuai0677 <18233561801@163.com>
2016-05-03 20:30:56 +0800

    TreeADT

--- tree/binTree.java
+++ tree/binTree.java
@@ -1,50 +1,52 @@
 package tree;
 
 public class TreeADT {
-	
-	        private String data=null;  
-	        private TreeADT  leftChild=null;  
-	        private TreeADT  rightChild=null;      
-	      
-	        public TreeADT (String data){  
-	            this.data=data;  
-	            this.leftChild=null;  
-	            this.rightChild=null;  
-	        }  
-	    
-	   
-	 		
-	 		public void createBinTree(TreeADT  root){  
-	 			TreeADT  newNodeB = new TreeADT ("B");  
-	 	        TreeADT  newNodeC = new TreeADT ("C");  
-	 	        TreeADT  newNodeD = new TreeADT ("D");  
-	 	        TreeADT  newNodeE = new TreeADT ("E");  
-	 	        TreeADT newNodeF = new TreeADT ("F");  
-	 	        root.leftChild=newNodeB;  
-	 	        root.rightChild=newNodeC;  
-	 	        root.leftChild.leftChild=newNodeD;  
-	 	        root.leftChild.rightChild=newNodeE;  
-	 	        root.rightChild.rightChild=newNodeF;  
-	 	    }  
-	 		public void visted(TreeADT subTree){   
-	 	        System.out.println(subTree.data);;  
-	 	    }  
-	 		public void preOrder(TreeADT subTree){  
-	 	        if(subTree!=null){  
-	 	            visted(subTree);  
-	 	            preOrder(subTree.leftChild);  
-	 	            preOrder(subTree.rightChild);  
-	 	        }  
-	 	    }  
-	       public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		       TreeADT  root =new TreeADT ("A");
-		       root.createBinTree(root);
-		       root.preOrder(root);
-		//System.out.println(root);
-		
+	private String data=null;  
+    private TreeADT  leftChild=null;  
+    private TreeADT  rightChild=null;      
+  
+    public TreeADT (String data){
+    	
+        this.data=data;  
+        this.leftChild=null;  
+        this.rightChild=null;  
+    }  
 
-	}
+    //创建一颗二叉树
+	public void createBinTree(TreeADT  root){  
+		TreeADT  newNodeB = new TreeADT ("B");  
+        TreeADT  newNodeC = new TreeADT ("C");  
+        TreeADT  newNodeD = new TreeADT ("D");  
+        TreeADT  newNodeE = new TreeADT ("E");  
+        TreeADT newNodeF = new TreeADT ("F");  
+        root.leftChild=newNodeB;  
+        root.rightChild=newNodeC;  
+        root.leftChild.leftChild=newNodeD;  
+        root.leftChild.rightChild=newNodeE;  
+        root.rightChild.rightChild=newNodeF;  
+    }  
+	//访问节点数据
+	public void visted(TreeADT subTree){   
+		System.out.println(subTree.data);;  
+	    }
+	//前序递归遍历
+	public void preOrder(TreeADT subTree){  
+		 if(subTree!=null){  
+	            visted(subTree);  
+	            preOrder(subTree.leftChild);  
+	            preOrder(subTree.rightChild);  
+	        }  
+	    }  
+	       
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+    	TreeADT  root =new TreeADT ("A");
+        root.createBinTree(root);
+        root.preOrder(root);
 
 }
 
+}
+
+	
+	        
